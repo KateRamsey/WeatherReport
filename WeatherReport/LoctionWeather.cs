d2758c09e6dd2e4b4eb1b8105973d0ff89b442a1
Kate Ramsey <kateramsey@live.com>
2016-03-23 13:43:11 -0500

    Added classes generated by Json2CSharp.com

---
+++ WeatherReport/LoctionWeather.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+
+namespace WeatherReport
+{
+    public class LoctionWeather
+    {
+        public Response response { get; set; }
+        public CurrentObservation current_observation { get; set; }
+        public Forecast forecast { get; set; }
+        public MoonPhase moon_phase { get; set; }
+        public SunPhase sun_phase { get; set; }
+        public string query_zone { get; set; }
+        public List<Alert> alerts { get; set; }
+        public List<object> currenthurricane { get; set; }
+    }
+}
