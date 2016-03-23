5286afa4a33ff16a9931766ec505e5e11a24f5a0
Kate Ramsey <kateramsey@live.com>
2016-03-23 15:13:19 -0500

    Made WeatherReport class to deal with formatting output

--- WeatherReport/Program.cs
+++ WeatherReport/Program.cs
@@ -11,11 +11,17 @@ namespace WeatherReport
     {
         static void Main(string[] args)
         {
+
+            //TODO: Database
+
+
             int Zip = 0;
             string APIkey = "4815a8bfa3e118cb";
             Console.WriteLine("What location would you like to get the weather for?");
             string ZipCodeRegEx = "^([0-9]{5})$";
             string userInput = Console.ReadLine();
+
+            //TODO: better user input check
             if (userInput == ZipCodeRegEx)
             {
                 Zip = Int32.Parse(userInput);
@@ -36,13 +42,53 @@ namespace WeatherReport
             }
             var response = client.Execute<LocationWeather>(request);
             var LocalWeatherInfo = response.Data;
+            WeatherReport weatherReport = new WeatherReport();
 
-            Console.WriteLine($"At Zip Code {LocalWeatherInfo.current_observation.display_location.zip} ({LocalWeatherInfo.current_observation.display_location.city}," +
-                              $" {LocalWeatherInfo.current_observation.display_location.state})" +
-                              $" it feels like {LocalWeatherInfo.current_observation.feelslike_string}");
 
+            //TODO: Write Output Functions
+            weatherReport.PrintCurrentConditions(LocalWeatherInfo);
+            weatherReport.Print10DayForecast(LocalWeatherInfo);
+            weatherReport.PrintSunrise(LocalWeatherInfo);
+            weatherReport.PrintSunset(LocalWeatherInfo);
+            weatherReport.PrintCurrentAlerts(LocalWeatherInfo);
+            weatherReport.PrintActiveHurricanes(LocalWeatherInfo);
 
             Console.ReadLine();
+        }
+    }
+
+    public class WeatherReport
+    {
+        public void PrintCurrentConditions(LocationWeather lw)
+        {
+            Console.WriteLine($"At Zip Code {lw.current_observation.display_location.zip} ({lw.current_observation.display_location.city}," +
+                  $" {lw.current_observation.display_location.state})" +
+                  $" it feels like {lw.current_observation.feelslike_string}");
+        }
+
+        public void Print10DayForecast(LocationWeather localWeatherInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintSunrise(LocationWeather localWeatherInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintSunset(LocationWeather localWeatherInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintCurrentAlerts(LocationWeather localWeatherInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintActiveHurricanes(LocationWeather localWeatherInfo)
+        {
+            throw new NotImplementedException();
         }
     }
 
