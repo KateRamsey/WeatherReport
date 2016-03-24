30f0a13889a9d1d2cb14891313d82a350c90d14a
Kate Ramsey <kateramsey@live.com>
2016-03-23 23:29:05 -0500

    Started to add needed fields to WeatherInfo (a simplified view of the weather)

--- WeatherReport/WeatherInfo.cs
+++ WeatherReport/WeatherInfo.cs
@@ -2,5 +2,16 @@
 {
     public class WeatherInfo
     {
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string CurrentFeelsLike { get; set; }
+        public string SunsetHour { get; set; }
+        public string SunsetMinute { get; set; }
+        public string SunriseHour { get; set; }
+        public string SunriseMinute { get; set; }
+        //10 day forecast
+        //alerts
+        //huricane
     }
 }
